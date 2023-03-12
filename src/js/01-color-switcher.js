e23f5d0a687ff5dc06f83e719fb299501fc91cfd
Roman Usenko <luckyjuse@gmail.com>
2023-03-12 23:54:22 +0200

    9.1 finished

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+const TIMER_DELAY = 1000;
+let intervalId = null;
+
+refs.start.addEventListener('click', changeColor);
+
+refs.stop.addEventListener('click', stopChangeColor);
+
+refs.stop.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangeColor() {
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+  clearInterval(intervalId);
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,16 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const timerDiv = document.querySelector('#datetime-picker');
+
+flatpickr(timerDiv, options);
