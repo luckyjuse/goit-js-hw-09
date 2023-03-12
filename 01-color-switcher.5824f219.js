1406caa2492bb254adaf7650409376d1f9b4c2dd
luckyjuse <107860445+luckyjuse@users.noreply.github.com>
2023-03-12 21:54:48 +0000

    Deploying to gh-pages from @ luckyjuse/goit-js-hw-09@e23f5d0a687ff5dc06f83e719fb299501fc91cfd 🚀

---
+++ 01-color-switcher.5824f219.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=null;t.start.addEventListener("click",(function(){t.start.disabled=!0,t.stop.disabled=!1,e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(function(){t.start.disabled=!1,t.stop.disabled=!0,clearInterval(e)})),t.stop.disabled=!0;
+//# sourceMappingURL=01-color-switcher.5824f219.js.map
