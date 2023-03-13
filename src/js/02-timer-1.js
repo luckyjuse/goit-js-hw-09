1bc8c5e5d43dedd51f61b48a8291dfe607b68c88
Roman Usenko <luckyjuse@gmail.com>
2023-03-14 01:08:16 +0200

    9.2 added

---
+++ src/js/02-timer-1.js
@@ -0,0 +1,77 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  start: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
+let selectedTime = null;
+let intervalId = null;
+refs.start.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= Date.now()) {
+      Notify.failure('Please choose a date in the future');
+      refs.start.disabled = true;
+    } else {
+      refs.start.disabled = false;
+      selectedTime = selectedDates[0];
+    }
+  },
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+function runTimer(params) {
+  const remainingTime = selectedTime - Date.now();
+
+  if (remainingTime < 0) {
+    clearInterval(intervalId);
+    return;
+  }
+  render(convertMs(remainingTime));
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function onBtnClick() {
+  refs.start.disabled = true;
+  refs.input.disabled = true;
+  intervalId = setInterval(runTimer, 1000);
+}
+
+function render(time) {
+  const { days, hours, minutes, seconds } = time;
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+}
+
+refs.start.addEventListener('click', onBtnClick);
+flatpickr('#datetime-picker', options);
